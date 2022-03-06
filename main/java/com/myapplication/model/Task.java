f1f34aefae5ee3dacffcfc797d7f9ff61e8a7406
Akkireddie <99355213+Akkireddie@users.noreply.github.com>
2022-03-06 17:04:25 +0500

    Add files via upload

---
+++ main/java/com/myapplication/model/Task.java
@@ -0,0 +1,71 @@
+package com.myapplication.model;
+
+/**
+ * Created by Akhila on 2022-02-27.
+ */
+
+public class Task {
+    private int id;
+    private int action;
+    private String date;
+    private String taskDesc;
+    private String backColor;
+
+    public Task() {
+    }
+
+    public Task(String date, String taskDesc, String backColor,int action) {
+        this.date = date;
+        this.taskDesc = taskDesc;
+        this.backColor = backColor;
+        this.action = action;
+    }
+
+    public Task(int id, String date, String taskDesc, String backColor,int action) {
+        this.id = id;
+        this.date = date;
+        this.taskDesc = taskDesc;
+        this.backColor = backColor;
+        this.action = action;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTaskDesc() {
+        return taskDesc;
+    }
+
+    public void setTaskDesc(String taskDesc) {
+        this.taskDesc = taskDesc;
+    }
+
+    public String getBackColor() {
+        return backColor;
+    }
+
+    public void setBackColor(String backColor) {
+        this.backColor = backColor;
+    }
+
+    public int getAction() {
+        return action;
+    }
+
+    public void setAction(int action) {
+        this.action = action;
+    }
+}
